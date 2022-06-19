7bc19f61ed07e0d45a6f39752e8bfb5c8b69240b
brendena <badamczak@creativemicro.com>
2022-06-19 16:01:59 -0400

    stuff starting to shape up

---
+++ buildDep.sh
@@ -0,0 +1,3 @@
+cd MathEquations-component
+npm run build
+mv build/direflowBundle.js ../src/MathEquationComponent.js
---
+++ init.sh
@@ -0,0 +1,5 @@
+cd MathEquations-component
+npm install
+cd mathjax
+npm install
+npm run build
